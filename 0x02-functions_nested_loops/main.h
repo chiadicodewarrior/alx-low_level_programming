b5abfb243f2a4eeba69301a5bc47edf1e553b4b2
chiadicodewarrior <paulchiadikaobi@gmail.com>
2023-02-21 15:08:09 -0800

    Write a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,7 @@
-/* This a header file containting declarations 
- * for prototype functions used in the 0x02-functions_nested_loops
- * task and directory
- * THis is like creating my own library with declared functions
- */
-int main(void);
-int _putchar(int);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,6 +11,9 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
+void num(int r, int n);
+
+#endif
